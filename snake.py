031cdd4a3366bc79bd50e63e96b136516ef28f4b
Flo <93609174+Xeferis@users.noreply.github.com>
2024-11-12 20:24:23 +0100

    Try to fix Tail

--- snake.py
+++ snake.py
@@ -7,6 +7,8 @@ class snake:
         self.canvas = canvas
         self.snake = None
         self.speed = 100
+        self.last_pos = []
+        self.tail = []
         self.collected = 0
         self.x = 0
         self.y = 0
@@ -15,6 +17,7 @@ class snake:
         self.snake = self.canvas.create_rectangle(0, 0, 10, 10, fill="black")
 
     def move(self):
+        self.last_pos = [self.canvas.coords(self.snake)]
         self.canvas.move(self.snake, self.x, self.y)
         # ? Uncomment the following lines to make the snake move continuously
         # self.x = 0
@@ -27,7 +30,6 @@ class snake:
         )
 
     def border_collision(self):
-        print(self.canvas.coords(self.snake))
         if len(self.canvas.coords(self.snake)) <= 0:
             return True
         elif self.canvas.coords(self.snake)[0] < 0:
@@ -39,18 +41,21 @@ class snake:
         elif self.canvas.coords(self.snake)[3] > 500:
             self.kill()
 
+    def clean_pos(self):
+        if len(self.tail) < len(self.last_pos):
+            self.last_pos.pop(-1)
+
     def speed_up(self):
         self.speed -= 10
 
+    def update_tail(self):
+        for i in range(len(self.tail)):
+            self.canvas.move(self.tail[i], self.last_pos[i][0], self.last_pos[i][1])
+
     def grow(self):
-        # ! NOT WORKING YET
-        self.canvas.create_rectangle(
-            self.canvas.coords(self.snake)[0],
-            self.canvas.coords(self.snake)[1],
-            self.canvas.coords(self.snake)[2],
-            self.canvas.coords(self.snake)[3],
-            fill="black",
-        )
+        x = self.last_pos[0][0]
+        y = self.last_pos[0][1]
+        self.tail.append(self.canvas.create_rectangle(x, y, x + 10, y + 10, fill="black"))
 
     def collect_food(self):
         if self.canvas.coords(self.snake) == self.canvas.coords(self.food):
@@ -66,26 +71,23 @@ class snake:
         self.food = self.canvas.create_rectangle(x, y, x + 10, y + 10, fill="red")
 
     def update(self):
+        self.clean_pos()
+        print(self.last_pos)
         if not self.border_collision():
             self.move()
             self.collect_food()
             self.canvas.after(self.speed, self.update)
 
     def control(self, event):
-        print(event.keysym)
         if event.keysym == "Up":
-            print("move up")
             self.y = -10
             self.x = 0
         elif event.keysym == "Down":
-            print("move down")
             self.y = 10
             self.x = 0
         elif event.keysym == "Left":
-            print("move left")
             self.x = -10
             self.y = 0
         elif event.keysym == "Right":
-            print("move right")
             self.x = 10
             self.y = 0
