34c041b7eb118e3d783e0db1a06cda24a22f87e8
Florian <93609174+Xeferis@users.noreply.github.com>
2024-11-13 07:40:38 +0100

    Added Std

--- main.py
+++ main.py
@@ -7,9 +7,5 @@ root.resizable(False, False)
 
 canvas = tkinter.Canvas(root, width=500, height=500)
 canvas.pack()
-s = snake(canvas)
-s.create_snake()
-s.generate_food()
-root.bind("<KeyPress>", lambda event: s.control(event))
-s.update()
+
 root.mainloop()
